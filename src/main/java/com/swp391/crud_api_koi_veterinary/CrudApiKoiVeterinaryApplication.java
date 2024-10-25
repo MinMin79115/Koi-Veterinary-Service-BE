8d412ccc286b0eb329146bd2bd358f640a013460
vnthuan01 <>
2024-10-25 12:43:54 +0700

    swagger and fix response payment
    payment trả về trang thành công nếu thành toán thành công hoặc hủy thì về lại trang lịch sử

--- src/main/java/com/swp391/crud_api_koi_veterinary/CrudApiKoiVeterinaryApplication.java
+++ src/main/java/com/swp391/crud_api_koi_veterinary/CrudApiKoiVeterinaryApplication.java
@@ -3,9 +3,12 @@ package com.swp391.crud_api_koi_veterinary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.swp391.crud_api_koi_veterinary"})
+@OpenAPIDefinition(info = @Info(title = "Koi Veterinary API", version = "1.0", description = "API documentation for Koi Veterinary Service"))
 public class CrudApiKoiVeterinaryApplication {
 
     public static void main(String[] args) {
